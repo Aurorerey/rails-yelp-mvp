76b34d8c4409045244105c44ba5eac6098887f66
Aurore Rey <rey.pandora@gmail.com>
2021-03-03 15:22:14 +0100

    view index

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Cleaning database..."
-Restaurant.destroy_all
-
-puts "Creating restaurants..."
-dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "06,21,22,22,22", category: "chinese" }
-pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "09,67,67,67", category: "irish" }
-
-[ dishoom, pizza_east ].each do |attributes|
-  restaurant = Restaurant.create!(attributes)
-  puts "Created #{restaurant.name}"
-end
-puts "Finished!"
+Restaurant1 = Restaurant.new(name: ‘LAmbroisie’, address: ‘10 rue de Bellevue’, phone_number: ‘0145667889’, category: ‘french’)
+Restaurant2 = Restaurant.new(name: ‘La petite Sauterelle’, address: ‘22 rue des beaux’, phone_number: ‘0145767889’, category: ‘japanese’)
+Restaurant3 = Restaurant.new(name: ‘Le petit Nice’, address: ‘99 rue paradis’, phone_number: ‘0145687889’, category: ‘chinese’)
+Restaurant4 = Restaurant.new(name: ‘Ratatouille’, address: ‘10 avenue Jean Boulier’, phone_number: ‘0145669889’, category: ‘belgian’)
+Restaurant5 = Restaurant.new(name: ‘Le Reptilien’, address: ‘1 rue des jarlats’, phone_number: ‘0545667889’, category: ‘italian’)
