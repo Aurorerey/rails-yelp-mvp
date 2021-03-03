85850489d0c08f762c556bea393a057003eea4a2
Aurore Rey <rey.pandora@gmail.com>
2021-03-03 15:39:44 +0100

    controller restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,17 +3,38 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
-
   def show
     @restaurant = Restaurant.find(params[:id])
   end
-
   def new
     @restaurant = Restaurant.new
   end
-
+  # GET /restaurants/1/edit
+  def edit
+  end
+  # POST /restaurants
   def create
     @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant, notice: ‘Restaurant was successfully created.’
+    else
+      render :new
+    end
   end
-
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: ‘Restaurant was successfully updated.’
+    else
+      render :edit
+    end
+  end
+  # DELETE /restaurants/1
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: ‘Restaurant was successfully destroyed.’
+  end
+private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
