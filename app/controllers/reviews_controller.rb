2a411ccb155655da498606cf4c38a2ce7532824c
Aurore Rey <rey.pandora@gmail.com>
2021-03-03 16:22:15 +0100

    fallait pas rater mon setuuup

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,6 @@ class ReviewsController < ApplicationController
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
-
   end
 
   def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
 
-Restaurant1 = Restaurant.new(name: ‘LAmbroisie’, address: ‘10 rue de Bellevue’, phone_number: ‘0145667889’, category: ‘french’)
-Restaurant2 = Restaurant.new(name: ‘La petite Sauterelle’, address: ‘22 rue des beaux’, phone_number: ‘0145767889’, category: ‘japanese’)
-Restaurant3 = Restaurant.new(name: ‘Le petit Nice’, address: ‘99 rue paradis’, phone_number: ‘0145687889’, category: ‘chinese’)
-Restaurant4 = Restaurant.new(name: ‘Ratatouille’, address: ‘10 avenue Jean Boulier’, phone_number: ‘0145669889’, category: ‘belgian’)
-Restaurant5 = Restaurant.new(name: ‘Le Reptilien’, address: ‘1 rue des jarlats’, phone_number: ‘0545667889’, category: ‘italian’)
+
+Restaurant1 = Restaurant.create(name: ‘LAmbroisie’, address: ‘10 rue de Bellevue’, phone_number: ‘0145667889’, category: ‘french’)
+Restaurant2 = Restaurant.create(name: ‘La petite Sauterelle’, address: ‘22 rue des beaux’, phone_number: ‘0145767889’, category: ‘japanese’)
+Restaurant3 = Restaurant.create(name: ‘Le petit Nice’, address: ‘99 rue paradis’, phone_number: ‘0145687889’, category: ‘chinese’)
+Restaurant4 = Restaurant.create(name: ‘Ratatouille’, address: ‘10 avenue Jean Boulier’, phone_number: ‘0145669889’, category: ‘belgian’)
+Restaurant5 = Restaurant.create(name: ‘Le Reptilien’, address: ‘1 rue des jarlats’, phone_number: ‘0545667889’, category: ‘italian’)
